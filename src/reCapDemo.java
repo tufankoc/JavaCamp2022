028654c811c2cbad9419ea7b2ab80b071c4bcc32
Tufan Koc <tufankoc00@gmail.com>
2022-09-23 10:05:29 +0300

    1-week

---
+++ src/reCapDemo.java
@@ -0,0 +1,14 @@
+public class reCapDemo {
+    int sayi1 = 10;
+    int sayi2 = 91;
+    int sayi3 = 20;
+    int enBuyuk = sayi1;
+
+     if (enBuyuk<sayi2){
+        enBuyuk = sayi2;
+    }if (enBuyuk < sayi3){
+        enBuyuk = sayi3;
+    }
+
+    System.out.println(enBuyuk);
+}
